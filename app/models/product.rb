61a149b387e2b8b50e97dd2a3f92eaab4889fca2
Dan Solo <dan@solocreative.com.au>
2015-09-13 20:05:00 +1000

    Attempt to implement caching but does not work

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,8 @@ class Product < ActiveRecord::Base
   validates :title, uniqueness: true
   validates_length_of :title, :minimum => 10
   validates :image_url, allow_blank: true, format:{  with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG PNG image'}
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
