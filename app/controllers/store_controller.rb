bc0f7496ec2e8d3b65868a9270633f89b8256299
Dan Solo <dan@solocreative.com.au>
2015-09-16 08:22:57 +1000

    setup cart sessions and add a couple of sneaky counts etc to catalog page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
   def index
+    if session[:counter].nil?
+      @count = session[:counter] = 1
+    else
+      @count = session[:counter] += 1
+    end
     @products = Product.order(:title)
   end
 end
